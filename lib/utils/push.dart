9f8222b4cde2c73c1f97dd3e4c423d974a0faa4f
tlinxe <linxe@reflexe.fr>
2023-06-19 12:29:32 +0200

    More small changes and corrections

--- lib/utils/push.dart
+++ lib/utils/push.dart
@@ -1,66 +1,35 @@
-import 'dart:convert';
-import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart' as foundation;
+import 'package:flutter/material.dart';
 
 // Internal package
 import 'package:bab/models/event_model.dart';
-import 'package:bab/utils/constants.dart';
 
 // External package
-import 'package:googleapis_auth/auth_io.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 
 class Push {
-  static Future<void> send(EventModel model, {String topic = 'default'}) async {
-    try {
-      String jsonContent = await rootBundle.loadString('assets/data/service_account.json');
-      final jsonFile = json.decode(jsonContent);
-      final accountCredentials = ServiceAccountCredentials.fromJson(jsonFile);
+  static Future<void> notification(BuildContext context, EventModel model, {String topic = 'default'}) async {
+    HttpsCallable callable = FirebaseFunctions.instance.httpsCallable(
+      'notification',
+      options: HttpsCallableOptions(
+        timeout: const Duration(seconds: 5),
+      ),
+    );
 
-      List<String> scopes = ["https://www.googleapis.com/auth/cloud-platform"];
-      clientViaServiceAccount(accountCredentials, scopes).then((AuthClient client) async {
-        var body = _notification(model, topic);
-        print('[$APP_NAME] Post request: $body');
-        var response = await client.post(
-          Uri.parse('https://fcm.googleapis.com/v1/projects/$PROJECT_ID/messages:send'),
-          headers: <String, String>{
-            'Content-Type': 'application/json; charset=UTF-8',
-            'Authorization': 'Bearer ${client.credentials.accessToken}'
-          },
-          body: body,
-        );
-        if (response.statusCode != 200) {
-          print('[$APP_NAME] Post ERROR: ${response.body}');
-          throw new Exception(response.body);
-        }
+    try {
+      await callable.call(<String, dynamic>{
+        'topic': foundation.kDebugMode ? 'debug' : 'default',
+        'title': model.title,
+        'subtitle': model.subtitle,
+        'uuid': model.uuid,
+        'model': 'event'
       });
     } catch (e) {
-      throw new Exception(e.toString());
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('ERROR: $e'),
+        ),
+      );
     }
-  }
-
-  static String _notification(EventModel model, String topic) {
-    return jsonEncode({
-      'message': {
-        'token': '/topics/$topic',
-        'notification': {
-          'title': model.title,
-          'body': model.subtitle,
-        },
-        'android':{
-          'priority': 'normal',
-          'notification': {
-            'channel_id': channelId,
-          },
-        },
-        'apns':{
-          'headers': {
-            'apns-priority': '5'
-          }
-        },
-        'data': {
-          'id': model.uuid,
-          'name': 'event'
-        },
-      }
-    });
   }
 }
